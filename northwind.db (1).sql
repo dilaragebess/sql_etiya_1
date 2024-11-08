fc4c1de7f810d8a56ef026de2db0dbfb08800a19
dilaragebess <dilara.gebes@etiya.com>
2024-11-08 18:21:36 +0300

    Update northwind.db (1).sql

--- northwind.db (1).sql
+++ northwind.db (1).sql
@@ -1,3 +1,29 @@
+SQL Workshop 1 Questions:
+ 
+Tedarikçi ID'si 1 ile 5 arasındaki ürünler:
+Tedarikçi ID'si 1, 2, 3, 4 veya 5 olan ürünleri listeleyin.
+Tedarikçi ID'si 1, 2, 4 veya 5 olan ürünler:
+Tedarikçi ID'si 1, 2, 4 veya 5 olan ürünleri listeleyin.
+Ürün adı 'Chang' veya 'Aniseed Syrup' olan ürünler:
+Ürün adı 'Chang' veya 'Aniseed Syrup' olan ürünleri listeleyin.
+Tedarikçi ID'si 3 olan veya birim fiyatı 10'dan büyük ürünler:
+Tedarikçi ID'si 3 olan veya birim fiyatı 10'dan büyük olan ürünleri listeleyin.
+Ürün adı ve birim fiyatı ile birlikte getirme:
+Ürün adı ve birim fiyatını içeren listeyi getirin.
+Büyük harfe dönüştürerek 'c' harfi içeren ürünler:
+Ürün adlarını büyük harfe dönüştürdükten sonra 'c' harfi içeren ürünleri listeleyin. (örneğin: 'Chai', 'Chocolate', vs.)
+'n' ile başlayan ve tek karakterli bir harf içeren ürünler:
+Ürün adı 'n' harfi ile başlayan ve içerisinde tek karakterli bir harf içeren ürünleri listeleyin. (örneğin: 'Naai, 'Nectar', vs.)
+Stok miktarı 50'den fazla olan ürünler:
+Stok miktarı 50'den fazla olan ürünleri listeleyin.
+Ürünlerin en yüksek ve en düşük birim fiyatları:
+En yüksek ve en düşük birim fiyatına sahip ürünleri listeleyin.
+Ürün adında 'Spice' kelimesi geçen ürünler:
+Ürün adında 'Spice' kelimesi geçen ürünleri listeleyin.
+
+
+Answers:
+ 
 SELECT * FROM Products where supplierid BETWEEN 1 AND 5
 
 SELECT * FROM Products where supplierid IN (1,2,4,5)
